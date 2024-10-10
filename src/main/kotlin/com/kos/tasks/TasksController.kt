db3c45c00fe260e049c0dd45124fe508ca81e946
eric lopez <e.4p.bigdata.tid@telefonica.com>
2024-10-10 14:13:29 +0200

    Launch async the runTask and not wait for it

--- src/main/kotlin/com/kos/tasks/TasksController.kt
+++ src/main/kotlin/com/kos/tasks/TasksController.kt
@@ -6,15 +6,23 @@ import com.kos.common.ControllerError
 import com.kos.common.NotAuthorized
 import com.kos.common.NotEnoughPermissions
 import com.kos.credentials.CredentialsService
-import com.kos.roles.Role
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class TasksController(private val tasksService: TasksService, private val credentialsService: CredentialsService) {
+
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     suspend fun runTask(client: String?, taskRequest: TaskRequest): Either<ControllerError, Unit> {
         return when (client) {
             null -> Either.Left(NotAuthorized())
             else -> {
                 if (credentialsService.hasPermissions(client, Activities.runTask)) {
-                    Either.Right(tasksService.runTask(taskRequest.type))
+                    scope.launch {
+                        tasksService.runTask(taskRequest.type)
+                    }
+                    Either.Right(Unit)
                 } else Either.Left(NotEnoughPermissions(client))
             }
         }
